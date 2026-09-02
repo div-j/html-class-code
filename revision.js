5ecf1b71c6d3f93eb6694889010f4adb76f7cdca
div-j <divinejohn65@gmail.com>
2026-09-02 15:29:16 +0100

    quiz app

---
+++ revision.js
@@ -0,0 +1,49 @@
+  const questions = [
+  {
+    question: "What is the largest planet in our solar system?",
+    category: "Astronomy",
+    options: [
+      "Mars",
+      "Venus",
+      "Jupiter",
+      "Saturn"
+    ],
+    answer: "Jupiter"
+  },
+
+  {
+    question: "What is the closest planet to the Sun?",
+    category: "Astronomy",
+    options: [
+      "Earth",
+      "Mercury",
+      "Venus",
+      "Mars"
+    ],
+    answer: "Mercury"
+  },
+
+  {
+    question: "How many continents are there?",
+    category: "Geography",
+    options: [
+      "5",
+      "6",
+      "7",
+      "8"
+    ],
+    answer: "7"
+  },
+
+  {
+    question: "What is the capital of Nigeria?",
+    category: "Geography",
+    options: [
+      "Lagos",
+      "Kano",
+      "Abuja",
+      "Ibadan"
+    ],
+    answer: "Abuja"
+  }
+];
